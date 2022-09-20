d3e11330b351b55ddd8efc00772644715c5c8dc1
Aditya Kumar <adityakumar@Adityas-MBP-3.T-mobile.com>
2022-09-20 12:43:47 -0400

    Added models in backend

--- ecommerce_be/ecommerce_be/ecommerce_be/settings.py
+++ ecommerce_be/ecommerce_be/ecommerce_be/settings.py
@@ -121,7 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = 'static/images'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
